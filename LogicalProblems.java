5bb117b8e6c1c8b1b8da73a98afa59cb2bf25e79
iamsr5675 <saurabhranjan5675@gmail.com>
2021-08-25 02:42:10 +0530

    Reverse Number

--- LogicalProblems.java
+++ LogicalProblems.java
@@ -3,31 +3,18 @@ package com.bridgelabz.LogicalProblems;
 import java.util.Scanner;
 public class LogicalProblems 
 {
-	public static void main(String args[])
+	public static void main(String[] args)
 	{
+		int n, r = 0;
 		Scanner sc = new Scanner(System.in);
-		  int i, n, m = 0, f = 0;      
-		  System.out.println("Enter the number:");
-		  n = sc.nextInt();
-		  m = n / 2;      
-		  if ( n == 0 || n == 1 )
-		  {  
-		   System.out.println(n+" is not Prime Number");      
-		  }
-		  else
-		  {  
-		   for(i=2;i<=m;i++)
-		   {      
-		    if(n%i==0)
-		    {      
-		     System.out.println(n+" is not Prime Number");      
-		     f = 1;      
-		     break;      
-		    }      
-		   }      
-		   if (f == 0)  
-		   {
-			   System.out.println(n+" is Prime Number"); }  
-		  }
-		}    
-		}   
+		System.out.println("Enter the number:");
+		n = sc.nextInt();  
+		while (n != 0)   
+		{  
+		int remainder = n % 10;  
+		r = r * 10 + remainder;  
+		n = n / 10;  
+		}  
+		System.out.println("Reverse number is: " + r);  
+		}  
+		}  
