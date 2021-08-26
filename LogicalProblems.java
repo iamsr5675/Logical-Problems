3c92bd5f76a435bee9628932a3aeac3690f74f8c
iamsr5675 <saurabhranjan5675@gmail.com>
2021-08-26 13:13:32 +0530

    Coupon Numbers

--- LogicalProblems.java
+++ LogicalProblems.java
@@ -3,18 +3,26 @@ package com.bridgelabz.LogicalProblems;
 import java.util.Scanner;
 public class LogicalProblems 
 {
-	public static void main(String[] args)
-	{
-		int n, r = 0;
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number:");
-		n = sc.nextInt();  
-		while (n != 0)   
-		{  
-		int remainder = n % 10;  
-		r = r * 10 + remainder;  
-		n = n / 10;  
-		}  
-		System.out.println("Reverse number is: " + r);  
-		}  
-		}  
+		System.out.println("Enter the no. of coupons:");
+		int n = sc.nextInt();
+		char[] s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
+
+		System.out.println(s[1]);
+
+		for (int i = 0; i < n; i++) {
+			int random = (int) (Math.random() * 999999999);
+															
+			StringBuffer sb = new StringBuffer();
+			while (random > 0) {
+				sb.append(s[random % s.length]);
+				random /= s.length;
+			}
+
+			String CouponCode = sb.toString();
+			System.out.println("Coupon Code: " + CouponCode);
+		}
+	}
+}
+        
